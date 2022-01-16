2bc90d9f7ca4b284d7142e5aa4b5a0eeca2331af
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2022-01-16 19:27:35 +0000

    Bump version v0.2.0 -> v0.2.0.post1

--- html_section/__init__.py
+++ html_section/__init__.py
@@ -52,7 +52,7 @@ from sphinx.writers.latex import LaTeXTranslator
 __author__: str = "Dominic Davis-Foster"
 __copyright__: str = "2021 Dominic Davis-Foster"
 __license__: str = "BSD License"
-__version__: str = "0.2.0"
+__version__: str = "0.2.0.post1"
 __email__: str = "dominic@davis-foster.co.uk"
 
 __all__ = [
